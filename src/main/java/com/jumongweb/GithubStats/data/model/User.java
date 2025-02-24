b7de2e9d0d16a56c956002713487820f41b418df
Lawal Toheeb <lawaltoheeb36@gmail.com>
2025-02-24 11:53:55 +0100

    security again

--- src/main/java/com/jumongweb/GithubStats/data/model/User.java
+++ src/main/java/com/jumongweb/GithubStats/data/model/User.java
@@ -1,7 +1,7 @@
 package com.jumongweb.GithubStats.data.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
+//import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Entity
